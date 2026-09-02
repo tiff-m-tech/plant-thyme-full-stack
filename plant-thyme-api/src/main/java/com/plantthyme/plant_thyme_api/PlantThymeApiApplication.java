875362709b2d7041c9115989f4da0e694ecd7493
Tiffany <274891081+tiff-m-tech@users.noreply.github.com>
2026-09-02 11:55:17 -0400

    Set up Spring Boot project with MySQL database connection

---
+++ plant-thyme-api/src/main/java/com/plantthyme/plant_thyme_api/PlantThymeApiApplication.java
@@ -0,0 +1,13 @@
+package com.plantthyme.plant_thyme_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantThymeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantThymeApiApplication.class, args);
+	}
+
+}
---
+++ plant-thyme-api/src/test/java/com/plantthyme/plant_thyme_api/PlantThymeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantthyme.plant_thyme_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantThymeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
