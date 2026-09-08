40e20eda5676021f83432c1d58426aff81cb330e
Tiffany <274891081+tiff-m-tech@users.noreply.github.com>
2026-09-08 15:10:28 -0400

    Add validation for for progresspicture model.

--- plant-thyme-api/src/main/java/com/plantthyme/plant_thyme_api/model/ProgressPicture.java
+++ plant-thyme-api/src/main/java/com/plantthyme/plant_thyme_api/model/ProgressPicture.java
@@ -2,6 +2,8 @@ package com.plantthyme.plant_thyme_api.model;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
 import java.time.LocalDate;
 
 @Entity
@@ -13,11 +15,25 @@ public class ProgressPicture {
 
     @ManyToOne
     @JoinColumn(name = "collection_plant_id")
+    @NotNull(message = "Collection plant is required.")
     private CollectionPlant collectionPlant;
 
+    @NotBlank(message = "Image path is required.")
+    @Size(max = 255, message = "Image path must be 255 characters or less.")
     private String imagePath;
+
+    @NotNull(message = "Picture date is required.") // When pic added, it auto uses today's date, should be able to change for older pictures
+    @PastOrPresent(message = "Picture date cannot be in the future.")
     private LocalDate pictureDate;
+
+    @Pattern( // this is for a dropdown selection, adding validation for postman
+            regexp = "Growth|Repotting|Pruning|Pest Treatment|Other",
+            message = "Update type must be Growth, Repotting, Pruning, Pest Treatment, or Other."
+    )
     private String updateType;
+
+    @Column(length = 1000) // database column
+    @Size(max = 1000, message = "Notes must be 1000 characters or less.")
     private String notes;
 
     public ProgressPicture() {
