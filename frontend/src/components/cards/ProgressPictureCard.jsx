0dbe24ce62775bb283684a360ed6253e3162699c
Tiffany <274891081+tiff-m-tech@users.noreply.github.com>
2026-09-10 16:22:31 -0400

    Update progress picture alt text.

--- frontend/src/components/cards/ProgressPictureCard.jsx
+++ frontend/src/components/cards/ProgressPictureCard.jsx
@@ -1,11 +1,12 @@
-import { altFromFileName } from "../../utils/altFromFileName";
-
-// Default for fileName is used by newly selected preview images that do not pass a filename yet. Will replace in Unit 2 when backend is added.
-export default function ProgressPictureCard({ src, date, fileName = "Progress Picture" }) {
+export default function ProgressPictureCard({ src, date }) {
     return (
         <div className="progress-picture-card">
             <div className="progress-picture-date">{date}</div>
-            <img src={src} alt={altFromFileName(fileName)} className="progress-picture" />
+            <img
+                src={src}
+                alt={`Progress picture of plant from ${date}`}
+                className="progress-picture"
+            />
         </div>
     );
 }
