db87581b5698fe115d2032d21f7f8a276a38d220
Tiffany <274891081+tiff-m-tech@users.noreply.github.com>
2026-09-15 17:09:54 -0400

    Replace clickable card div with a View Details button

--- frontend/src/components/features/ProgressGallery.jsx
+++ frontend/src/components/features/ProgressGallery.jsx
@@ -5,7 +5,6 @@ import { faCamera } from "@fortawesome/free-solid-svg-icons";
 import PageTitle from "../ui/PageTitle";
 import ProgressPictureCard from "../cards/ProgressPictureCard";
 import { getProgressPictures, uploadProgressPicture } from "../../services/api";
-
 import { SERVER_URL } from "../../constants";
 
 export default function ProgressGallery({ collectionPlantId }) {
@@ -74,7 +73,6 @@ export default function ProgressGallery({ collectionPlantId }) {
                 {pictures.map((progressPic) => (
                     <ProgressPictureCard
                         key={progressPic.id}
-                        fileName={progressPic.imagePath}
                         date={formatDate(progressPic.pictureDate)}
                         src={`${SERVER_URL}/uploads/progress-pictures/${progressPic.imagePath}`}
                         onClick={() => navigate(`/progress-picture/${progressPic.id}`)}
